daf534710a555ef1c6aba61647e039dcb2b1ceea
QueyingZZZ <puhao@kth.se>
2025-09-20 17:26:46 +0200

    Update shift_regiter.sv

--- q1/shift_regiter.sv
+++ q1/shift_regiter.sv
@@ -1,4 +1,4 @@
-module shift_register #(parameter N=4)
+module shifter_register #(parameter N=4)
                       (input logic clk,
                        input logic rst_n,
                        input logic serial_parallel,
@@ -8,6 +8,39 @@ module shift_register #(parameter N=4)
                        output logic [N-1:0] parallel_out,
                        output logic serial_out);
 
-//complete here
 
+logic [N-1:0] regdata; //define variable of register
+
+
+assign serial_out = regdata[0];
+assign parallel_out = regdata;
+
+
+
+always_ff@(posedge clk or negedge rst_n)begin                 
+
+if(!rst_n) begin
+
+regdata <= 'b0;
+
+end
+
+
+else if(!serial_parallel && load_enable) begin             //for serial in
+
+  regdata[N-2:0] <= regdata[N-1:1];                       //data input                      
+  regdata[N-1] <= serial_in;
+
+end
+
+
+else if(serial_parallel && load_enable)             //for parallel in
+begin
+  
+  regdata <= parallel_in;   //parallel output              
+
+end
+
+end
 endmodule
+
